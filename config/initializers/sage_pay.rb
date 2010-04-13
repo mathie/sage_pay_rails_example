8e5f1d6732f1c61613353af23f3875142c00e58f
Graeme Mathieson <mathie@woss.name>
2010-04-13 13:42:14 +0100

    Initializer for the sage_pay gem.

---
+++ config/initializers/sage_pay.rb
@@ -0,0 +1,17 @@
+# Configuration options for Sage Pay Server.
+
+# Our vendor name. Bad news, this one only works in the simulator, not
+# live. :-)
+SagePay::Server.default_registration_options[:vendor] = "rubaidh"
+
+# Notification URL for pingbacks. Unfortunately, the URL helpers don't work in
+# here, or this would be +notification_url+. On the plus side, it's submitted
+# as a POST, so we can pretend they meant to deliver a RESTful :create action.
+# :)
+SagePay::Server.default_registration_options[:vendor] = "https://sage-pay-rails-example.heroku.com/notification"
+
+# Set the mode globally. Normally, you'd probably want to have different
+# settings for different environments (eg :test in staging and :live in
+# production) but we're pushing everything through the simulator as it's an
+# example application.
+SagePay::Server.default_registration_options[:mode] = :simulator
