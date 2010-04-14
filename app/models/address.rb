b4607fc076d246e5e1ef0fa068d4140f495c07c2
Graeme Mathieson <mathie@woss.name>
2010-04-14 13:16:09 +0100

    Basic validations on the address model.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,14 @@
 class Address < ActiveRecord::Base
   belongs_to :country
   belongs_to :payment
+
+  validates_presence_of :first_names, :surname, :address_1, :city, :post_code, :country_id
+  validates_length_of :first_names, :maximum =>  20, :allow_blank => true
+  validates_length_of :surname,     :maximum =>  20, :allow_blank => true
+  validates_length_of :address_1,   :maximum => 100, :allow_blank => true
+  validates_length_of :address_2,   :maximum => 100, :allow_blank => true
+  validates_length_of :city,        :maximum =>  40, :allow_blank => true
+  validates_length_of :post_code,   :maximum =>  10, :allow_blank => true
+  validates_length_of :state,       :maximum =>   2, :allow_blank => true
+  validates_length_of :phone,       :maximum =>  20, :allow_blank => true
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,6 +8,30 @@ describe Address do
   end
 
   describe "validations" do
-    
+    it { should     validate_presence_of :first_names                   }
+    it { should     validate_length_of   :first_names,  :maximum =>  20 }
+
+    it { should     validate_presence_of :surname                       }
+    it { should     validate_length_of   :surname,      :maximum =>  20 }
+
+    it { should     validate_presence_of :address_1                     }
+    it { should     validate_length_of   :address_1,    :maximum => 100 }
+
+    it { should_not validate_presence_of :address_2                     }
+    it { should     validate_length_of   :address_2,    :maximum => 100 }
+
+    it { should     validate_presence_of :city                          }
+    it { should     validate_length_of   :city,         :maximum =>  40 }
+
+    it { should     validate_presence_of :post_code                     }
+    it { should     validate_length_of   :post_code,    :maximum =>  10 }
+
+    it { should     validate_presence_of :country                       }
+
+    it { should_not validate_presence_of :state                         }
+    it { should     validate_length_of   :state,        :maximum =>   2 }
+
+    it { should_not validate_presence_of :phone                         }
+    it { should     validate_length_of   :phone,        :maximum =>  20 }
   end
 end
