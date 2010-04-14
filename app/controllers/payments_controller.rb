ef7150b1d9f94c46d284700af4893dd6a6ee732e
Graeme Mathieson <mathie@woss.name>
2010-04-14 16:27:52 +0100

    OK, so it would seem that the correct place to build an empty associated address model is in the view.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,8 +1,2 @@
 class PaymentsController < InheritedResources::Base
-  def resource
-    returning super do |payment|
-      payment.build_billing_address if payment.billing_address.blank?
-      payment.build_delivery_address if payment.delivery_address.blank?
-    end
-  end
 end
