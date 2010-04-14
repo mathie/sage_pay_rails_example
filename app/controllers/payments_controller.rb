a107b7602148e6ca65664356754399beb80933d8
Graeme Mathieson <mathie@woss.name>
2010-04-14 16:15:48 +0100

    Ask for a billing and delivery address on the payment form.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,8 @@
 class PaymentsController < InheritedResources::Base
+  def resource
+    returning super do |payment|
+      payment.build_billing_address if payment.billing_address.blank?
+      payment.build_delivery_address if payment.delivery_address.blank?
+    end
+  end
 end
