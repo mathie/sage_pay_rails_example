620878c7bce5d346d11e5c4da9e77cdf549a1b25
Graeme Mathieson <mathie@woss.name>
2010-04-14 08:55:13 +0100

    Basic validations for the payment model.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,7 @@
 class Payment < ActiveRecord::Base
   belongs_to :currency
 
-  validates_presence_of :description
+  validates_presence_of :description, :currency_id, :amount
   validates_length_of :description, :maximum => 100, :allow_blank => true
+  validates_numericality_of :amount, :greater_than_or_equal_to => 0.01, :less_than_or_equal_to => 100_000, :allow_blank => true
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -24,4 +24,50 @@ describe Payment do
     end
   end
 
+  describe "on #currency (and #currency_id)" do
+    it "should validate its presence" do
+      payment = Factory.build(:payment, :currency => nil)
+      payment.should_not be_valid
+      payment.should have(1).error_on(:currency_id)
+    end
+  end
+
+  describe "on #amount" do
+    it "should validate its presence" do
+      payment = Factory.build(:payment, :amount => "")
+      payment.should_not be_valid
+      payment.should have(1).error_on(:amount)
+    end
+
+    it "should validate that it's a number" do
+      payment = Factory.build(:payment, :amount => "chickens")
+      payment.should_not be_valid
+      payment.should have(1).error_on(:amount)
+    end
+
+    it "should validate that the minimum value is 0.01" do
+      payment = Factory.build(:payment, :amount => "-3.57")
+      payment.should_not be_valid
+      payment.should have(1).error_on(:amount)
+
+      payment = Factory.build(:payment, :amount => "0.00")
+      payment.should_not be_valid
+      payment.should have(1).error_on(:amount)
+
+      payment = Factory.build(:payment, :amount => "0.01")
+      payment.should be_valid
+    end
+
+    it "should validate that the maximum value is 100,000.00" do
+      payment = Factory.build(:payment, :amount => "99999")
+      payment.should be_valid
+
+      payment = Factory.build(:payment, :amount => "100000")
+      payment.should be_valid
+
+      payment = Factory.build(:payment, :amount => "100000.01")
+      payment.should_not be_valid
+      payment.should have(1).error_on(:amount)
+    end
+  end
 end
