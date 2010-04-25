23b8d2bf3222fb1f2d20e588115b5d54d10bcfb8
Graeme Mathieson <mathie@woss.name>
2010-04-25 18:39:48 +0100

    Renamed classes in the sage_pay gem.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,6 +6,6 @@ class NotificationsController < InheritedResources::Base
     sage_pay_transaction = SagePayTransaction.record_notification_from_params(params)
     render :text => sage_pay_transaction.response(payment_url(sage_pay_transaction.payment))
   rescue Exception => e
-    render :text => SagePay::Server::TransactionNotificationResponse.new(:status => :error, :status_detail => "An error occurred: #{e.message}", :redirect_url => payments_url).response
+    render :text => SagePay::Server::NotificationResponse.new(:status => :error, :status_detail => "An error occurred: #{e.message}", :redirect_url => payments_url).response
   end
 end
