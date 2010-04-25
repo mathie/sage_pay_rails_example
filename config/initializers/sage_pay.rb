d27c5414e9f2c32bdfa05371b7124454abb3e941
Graeme Mathieson <mathie@woss.name>
2010-04-25 11:33:37 +0100

    Oops, it's the plural I was looking for. And we only have a create action.

--- config/initializers/sage_pay.rb
+++ config/initializers/sage_pay.rb
@@ -5,10 +5,10 @@
 SagePay::Server.default_registration_options[:vendor] = "rubaidh"
 
 # Notification URL for pingbacks. Unfortunately, the URL helpers don't work in
-# here, or this would be +notification_url+. On the plus side, it's submitted
+# here, or this would be +notifications_url+. On the plus side, it's submitted
 # as a POST, so we can pretend they meant to deliver a RESTful :create action.
 # :)
-SagePay::Server.default_registration_options[:notification_url] = "https://sage-pay-rails-example.heroku.com/notification"
+SagePay::Server.default_registration_options[:notification_url] = "https://sage-pay-rails-example.heroku.com/notifications"
 
 # Set the mode globally. Normally, you'd probably want to have different
 # settings for different environments (eg :test in staging and :live in
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :currencies, :countries, :notifications
+  map.resources :currencies, :countries
   map.resources :payments, :has_one => :sage_pay_transaction
+  map.resources :notifications, :only => [ :create ]
   map.root :controller => 'root'
 end
