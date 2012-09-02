77b66792ba29b47ec0af673fefe1d6f6caea4840
Kelly McLellan <kelly@mclellans.org>
2012-09-02 06:38:13 +0100

    Added a few comments. Not sure if the second lot is technically correct...

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,14 @@ class NotificationsController < InheritedResources::Base
   actions :create
 
   def create
+    #(See SagePayTransaction model)
+    # The following takes the notification params returned from SagePay
+    # it saves them in the database table: Sage_Pay_Transactions
+    # and returns the notification params as sage_pay_transaction
     sage_pay_transaction = SagePayTransaction.record_notification_from_params(params)
+    #(See SagePayTransaction model)
+    # Takes notification params from above
+    # The following returns a response to SagePay with a URL to payment/action where action is pay,defer,release etc (from SagePayTransactionsController)
     render :text => sage_pay_transaction.response(payment_url(sage_pay_transaction.payment))
   rescue Exception => e
     render :text => SagePay::Server::NotificationResponse.new(:status => :error, :status_detail => "An error occurred: #{e.message}", :redirect_url => payments_url).response
